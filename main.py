52252bcd1e455101f9955afe339ee8cee3c3e311
sarartwizard <72253650+sarartwizard@users.noreply.github.com>
2021-10-17 16:11:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -102,8 +102,8 @@ model = model_from_json(loaded_model_json)
 
 
 #model.load_weights('https://github.com/sarartwizard/Detector-depression/main/model_MLPCLASSIFER.h5')
-
-model.load_weights('https://github.com/sarartwizard/Detector-depression/main/model_MLPCLASSIFER.h5')
+tf.keras.models.load_model('https://github.com/sarartwizard/Detector-depression/main/model_MLPCLASSIFER.h5')
+#model.load_weights('https://github.com/sarartwizard/Detector-depression/main/model_MLPCLASSIFER.h5')
 #model.load_weights("model_MLPCLASSIFER.h5")
 
 print(" -------  The model is  loaded from disk  -------")
