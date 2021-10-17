e06a7df51c6c289ea9f1afb4a61a5df3e0eeca7c
sarartwizard <72253650+sarartwizard@users.noreply.github.com>
2021-10-18 01:08:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ for i in range(len(xnew)):
 
 
 
-history = model.fit(X_train, y_train , validation_data=(X_test,y_test),  epochs=175, batch_size= 1000)
+history = model.fit(X_train, y_train , validation_data=(X_test,y_test),  epochs=100, batch_size= 1000)
 
 # 
 # st.write(df)
